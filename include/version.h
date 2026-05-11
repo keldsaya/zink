eb7e0c72571f90c810c5e632855ea8f08f56f15c
keldsaya <keldsaya@gmail.com>
2026-05-11 22:07:04 +0800

    sync

---
+++ include/version.h
@@ -0,0 +1,13 @@
+#ifndef __VERSION_H
+#define __VERSION_H
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 0
+#define VERSION_PATCH 1
+#define VERSION_EXTRA -rc1
+#define VERSION_STR "0.0.1-rc1 (Program)"
+
+#define PROGRAM_VERSION "0.0.1-rc1"
+#define PROGRAM_NAME "Program"
+
+#endif /* __VERSION_H */
